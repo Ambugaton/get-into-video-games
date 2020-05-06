332fd38565d72c631b178dc9fd96486b794d57bb
Ambugaton <elliott.groll@gmail.com>
2020-05-05 20:44:03 -0500

    theme-changing home buttons js page added

---
+++ buttonTheme.js
@@ -0,0 +1,140 @@
+//                                                  //
+//        On-Click Color-Changing Functions         //
+//                                                  //
+
+
+// Playstation/Sony colors //
+
+function psFunction() {
+
+    //Background
+
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#00004d";
+
+    document.getElementsByTagName("hr")[0].style.borderColor = "#e6b800";
+
+    document.getElementsByTagName("hr")[1].style.borderColor = "#e6b800";
+
+    document.getElementsByTagName("hr")[2].style.borderColor = "#e6b800";
+
+    document.getElementsByTagName("hr")[3].style.borderColor = "#e6b800";
+
+    //Showcase Boxes
+
+    document.getElementById("showcase-box-1").style.backgroundColor = "#e6b800";
+    document.getElementById("showcase-box-1").style.borderColor = "#e6b800";
+
+    document.getElementById("showcase-box-2").style.backgroundColor = "#e6b800";
+    document.getElementById("showcase-box-2").style.borderColor = "#e6b800";
+
+    document.getElementById("showcase-box-3").style.backgroundColor = "#e6b800";
+    document.getElementById("showcase-box-3").style.borderColor = "#e6b800";
+
+    document.getElementById("showcase-box-4").style.backgroundColor = "#e6b800";
+    document.getElementById("showcase-box-4").style.borderColor = "#e6b800";
+
+    document.getElementById("showcase-box-5").style.backgroundColor = "#00004d";
+    document.getElementById("showcase-box-5").style.borderColor = "#e6b800";
+
+    document.getElementsByTagName("h3")[0].style.color = "#e6b800";
+}
+
+// Nintendo colors //
+
+function ninFunction() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+
+    document.getElementsByTagName("hr")[0].style.borderColor = "white";
+
+    document.getElementsByTagName("hr")[1].style.borderColor = "white";
+
+    document.getElementsByTagName("hr")[2].style.borderColor = "white";
+
+    document.getElementsByTagName("hr")[3].style.borderColor = "white";
+
+    //Showcase Boxes
+
+    document.getElementById("showcase-box-1").style.backgroundColor = "white";
+    document.getElementById("showcase-box-1").style.borderColor = "white";
+
+    document.getElementById("showcase-box-2").style.backgroundColor = "white";
+    document.getElementById("showcase-box-2").style.borderColor = "white";
+
+    document.getElementById("showcase-box-3").style.backgroundColor = "white";
+    document.getElementById("showcase-box-3").style.borderColor = "white";
+
+    document.getElementById("showcase-box-4").style.backgroundColor = "white";
+    document.getElementById("showcase-box-4").style.borderColor = "white";
+
+    document.getElementById("showcase-box-5").style.backgroundColor = "white";
+    document.getElementById("showcase-box-5").style.borderColor = "white";
+
+    document.getElementsByTagName("h3")[0].style.color = "black";
+}
+
+// XBox colors //
+
+function msFunction() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#1a1a1a";
+
+    document.getElementsByTagName("hr")[0].style.borderColor = "green";
+
+    document.getElementsByTagName("hr")[1].style.borderColor = "green";
+
+    document.getElementsByTagName("hr")[2].style.borderColor = "green";
+
+    document.getElementsByTagName("hr")[3].style.borderColor = "green";
+
+    //Showcase Boxes
+
+    document.getElementById("showcase-box-1").style.backgroundColor = "green";
+    document.getElementById("showcase-box-1").style.borderColor = "green";
+
+    document.getElementById("showcase-box-2").style.backgroundColor = "green";
+    document.getElementById("showcase-box-2").style.borderColor = "green";
+
+    document.getElementById("showcase-box-3").style.backgroundColor = "green";
+    document.getElementById("showcase-box-3").style.borderColor = "green";
+
+    document.getElementById("showcase-box-4").style.backgroundColor = "green";
+    document.getElementById("showcase-box-4").style.borderColor = "green";
+
+    document.getElementById("showcase-box-5").style.backgroundColor = "green";
+    document.getElementById("showcase-box-5").style.borderColor = "green";
+
+    document.getElementsByTagName("h3")[0].style.color = "white";
+
+}
+
+// PC custom style //
+
+function pcFunction() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+
+    document.getElementsByTagName("hr")[0].style.borderColor = "#00ccff";
+
+    document.getElementsByTagName("hr")[1].style.borderColor = "#00ccff";
+
+    document.getElementsByTagName("hr")[2].style.borderColor = "#00ccff";
+
+    document.getElementsByTagName("hr")[3].style.borderColor = "#00ccff";
+
+    //Showcase Boxes
+
+    document.getElementById("showcase-box-1").style.backgroundColor = "#333";
+    document.getElementById("showcase-box-1").style.borderColor = "#00ccff";
+
+    document.getElementById("showcase-box-2").style.backgroundColor = "#333";
+    document.getElementById("showcase-box-2").style.borderColor = "#00ccff";
+
+    document.getElementById("showcase-box-3").style.backgroundColor = "#333";
+    document.getElementById("showcase-box-3").style.borderColor = "#00ccff";
+
+    document.getElementById("showcase-box-4").style.backgroundColor = "#333";
+    document.getElementById("showcase-box-4").style.borderColor = "#00ccff";
+
+    document.getElementById("showcase-box-5").style.backgroundColor = "#333";
+    document.getElementById("showcase-box-5").style.borderColor = "#00ccff";
+
+    document.getElementsByTagName("h3")[0].style.color = "white";
+}
